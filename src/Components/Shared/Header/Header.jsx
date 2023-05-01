1225ba12b09e7e6098d70f3152fc4449e125ab43
Ashraful Islam <ashraful.islam@aamartaka.com>
2023-05-01 11:51:02 +0600

    Navbar Spacing Issue Fixed

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -33,10 +33,10 @@ const Header = () => {
                 </div>
 
                 {/* Right column for menu */}
-                <div className="flex items-center gap-x-2">
+                <div className="flex items-center gap-x-1">
                   <Link
                     href="/"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -47,7 +47,7 @@ const Header = () => {
 
                   <Link
                     href="/cars"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -57,7 +57,7 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/bookService"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -67,17 +67,17 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/about"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
                     <span className="relative group-hover:text-white transition duration-300 ease-in-out">
-                      About Us
+                      About
                     </span>
                   </Link>
                   <Link
                     href="/ourServices"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -87,7 +87,7 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/branches"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -97,7 +97,7 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/blogs"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -107,7 +107,7 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/contact"
-                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                     <span className="absolute w-full h-full"></span>
@@ -117,13 +117,13 @@ const Header = () => {
                   </Link>
                   <Link
                     href="/dashboard"
-                    className="text-white px-2 py-1 text-lg font-semibold duration-300 bg-orange-600 border border-orange-600 rounded-[4px] flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                    className="text-white px-2 py-1 text-lg font-semibold duration-300 bg-orange-600 border border-orange-600 rounded-[4px] flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                   >
                     <MdAdminPanelSettings className="text-2xl" />
                     <span>Dashboard</span>
                   </Link>
                 </div>
-                <div className="flex items-center gap-x-2">
+                <div className="flex items-center">
                   {user?.email && (
                     <img
                       className="w-9 h-9 rounded-full border border-orange-600"
@@ -137,27 +137,27 @@ const Header = () => {
                   {user?.email ? (
                     <button
                       onClick={handleSignOut}
-                      className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center gap-x-1 hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
+                      className="relative justify-center px-3 py-1 overflow-hidden font-semibold rounded-[4px] group text-lg duration-300 flex items-center hover:bg-white hover:text-orange-600 focus:text-orange-600 focus:border-orange-600"
                     >
                       <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-orange-600 rounded-full group-hover:w-48 group-hover:h-56"></span>
                       <span className="absolute w-full h-full"></span>
-                      <span className="relative flex items-center gap-x-1 group-hover:text-white transition duration-300 ease-in-out">
+                      <span className="relative flex items-center group-hover:text-white transition duration-300 ease-in-out">
                         <MdOutlineExitToApp className="text-2xl" />
                         <span>Logout</span>
                       </span>
                     </button>
                   ) : (
-                    <div className="flex items-center ml-4">
+                    <div className="flex items-center">
                       <Link
                         href="/login"
-                        className="px-3 py-1 text-lg font-medium duration-300 border border-transparent bg-white rounded-[3px] hover:bg-orange-600 text-black hover:text-white focus:border-b focus:border-b-orange-600 flex items-center gap-x-2"
+                        className="px-3 py-1 text-lg font-medium duration-300 border border-transparent bg-white rounded-[3px] hover:bg-orange-600 text-black hover:text-white focus:border-b focus:border-b-orange-600 flex items-center gap-x-1"
                       >
                         <FaLock className="text-xl" />
                         <span>Login</span>
                       </Link>
                       <Link
                         href="/signup"
-                        className="px-3 py-1 text-lg font-medium duration-300 border border-transparent bg-white rounded-[3px] hover:bg-orange-600 text-orange-600 hover:text-white focus:border-b focus:border-b-orange-600 flex items-center gap-x-2"
+                        className="px-3 py-1 text-lg font-medium duration-300 border border-transparent bg-white rounded-[3px] hover:bg-orange-600 text-orange-600 hover:text-white focus:border-b focus:border-b-orange-600 flex items-center gap-x-1"
                       >
                         <FaUserAlt className="text-xl" />
                         <span>Signup</span>
